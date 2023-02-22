4d1a7558c76ee742875e3971a5987e8d162a3c14
ridoy113 <almamounhridoy113@gmail.com>
2023-02-22 12:40:58 +0600

    make button and a 0 counter

--- src/pages/Child.js
+++ src/pages/Child.js
@@ -2,8 +2,18 @@ import React from "react";
 
 const Child = () => {
   return (
-    <div>
-      <h1 className="text-5xl font-bold underline">Hello world!</h1>
+    <div className="flex justify-center">
+      <div>
+        <h1 className="flex justify-center text-5xl text-[#06B6D4]">0</h1>
+        <div className="grid gap-4 grid-cols-2 mt-5">
+          <button className="px-4 py-1 border-4 border-indigo-600 text-2xl">
+            Increment
+          </button>
+          <button className="px-4 py-1 border-4 border-indigo-600 text-2xl">
+            Decrement
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
