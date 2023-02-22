d4a0a6bb740c48f3657f5da8469f250f7e7d96e7
ridoy113 <almamounhridoy113@gmail.com>
2023-02-22 12:55:24 +0600

    first count complite

--- src/pages/Child.js
+++ src/pages/Child.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Child = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="flex justify-center">
       <div>
-        <h1 className="flex justify-center text-5xl text-[#06B6D4]">0</h1>
+        <h1 className="flex justify-center text-5xl text-[#06B6D4]">{count}</h1>
         <div className="grid gap-4 grid-cols-2 mt-5">
-          <button className="px-4 py-1 border-4 border-indigo-600 text-2xl">
+          <button
+            className="px-4 py-1 border-4 border-indigo-600 text-2xl"
+            onClick={() => setCount(count + 1)}
+          >
             Increment
           </button>
-          <button className="px-4 py-1 border-4 border-indigo-600 text-2xl">
+          <button
+            className="px-4 py-1 border-4 border-indigo-600 text-2xl"
+            onClick={() => setCount((prvState) => prvState - 1)}
+          >
             Decrement
           </button>
         </div>
